2acd1243da301a9468f010ef4cd4d01346e491de
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-19 19:28:24 -0700

    Added routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 8888;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Route to send user to index.html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// Route to send user to notes.html
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 
 // Spin up server
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
