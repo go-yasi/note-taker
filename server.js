3c47a09abbc3c7427cdcdfb4aa5f48aa83fe28db
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-19 19:26:40 -0700

    Added middleware for data parsing

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8888;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 // Spin up server
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
