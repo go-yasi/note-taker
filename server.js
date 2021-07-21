fd117669dc3240f1cfdb79e1ffa8b3bf7a15d8d9
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-20 19:45:26 -0700

    Updated port and added express static to public folder

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-// Require express
+// Require dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8008;
+app.use(express.static('public'));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Route to send user to index.html
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '.public/index.html')));
 // Route to send user to notes.html
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '.public/notes.html')));
 
 // Spin up server
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
