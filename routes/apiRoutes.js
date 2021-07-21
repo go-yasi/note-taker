52c0218deb9f082bfbb77a95b9afcfa2d0840ea2
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-20 20:29:22 -0700

    Installed uniqid npm package

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
+const fs = require("fs");
+
+let notes = require('../db/db.json')
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = notes.length.toString();
 
+    notes.push(newNote);
+    fs.writeFile('../db/db.json', JSON.stringify(notes));
+    res.json(newNote);
+});
 
 module.exports = router;
