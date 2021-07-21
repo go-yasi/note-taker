7d7195e49a6ee4663ef12cee444da938bd5d4a0c
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-20 20:37:09 -0700

    Required uniqid in apiRoutes. created a saveNote funciton

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,30 @@
 const router = require('express').Router();
 const fs = require("fs");
+const uniqid = require('uniqid');
 
 let notes = require('../db/db.json')
+
+function saveNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return newNote;
+}
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = notes.length.toString();
+    // const newNote = req.body;
+    // newNote.id = notes.length.toString();
 
-    notes.push(newNote);
-    fs.writeFile('../db/db.json', JSON.stringify(notes));
-    res.json(newNote);
+    // notes.push(newNote);
+    // fs.writeFile('../db/db.json', JSON.stringify(notes));
+    // res.json(newNote);
 });
 
 module.exports = router;
